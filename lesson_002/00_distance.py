90eda01b527b074fbdcff95689c9a312389ce099
taliq <talliqou@gmail.com>
2022-03-03 17:40:52 +0300

    1) Добавила переменные под  значения (х, у) по формуле. 2) Ввела словарь ['Moscow'] и расстояние от ['Moscow'] до ['London'] ['Paris'], а также переменные moscow_london, moscow_paris. 3) Ввела словарь ['London'] и расстоение от ['London'] до ['Paris'], ['Moscow'] + переменные london_paris, moscow_london. 4) Ввела словарь ['Paris'] и расстояние от ['Paris'] до ['London'], ['Moscow'] + переменные london_paris и moscow_paris. 5) Поменяла print на pprint.

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
-
+from pprint import pprint
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -12,11 +12,30 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-distances = {}
+distances = dict()
 
-# TODO здесь заполнение словаря
 
-print(distances)
+Moscow = sites['Moscow']
+London = sites['London']
+Paris = sites['Paris']
+
+moscow_london = ((Moscow[0] - London[0])**2 + (Moscow[1] - London[1])**2) ** .5
+moscow_paris = ((Moscow[0] - Paris[0])**2 + (Moscow[1] - Paris[1])**2) ** .5
+london_paris = ((London[0] - Paris[0])**2 + (London[1]- Paris[1])**2) ** .5
+
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
+
+distances['London'] = {}
+distances['London']['Paris'] = london_paris
+distances['London']['Moscow'] = moscow_london
+
+distances['Paris'] = {}
+distances['Paris']['London'] = london_paris
+distances['Paris']['Moscow'] = moscow_paris
+
+pprint(distances)
 
 
 
